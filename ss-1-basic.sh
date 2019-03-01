5a00589813ea9037427ef3f3a968f5914524aef8
pc37utn <pc37utn@users.noreply.github.com>
2019-03-01 16:19:26 -0500

    Update ss-1-basic.sh

--- ss-1-basic.sh
+++ ss-1-basic.sh
@@ -35,19 +35,19 @@ yum -y install git vim
 if [$OS = "centos"]; then
   ## add openjdk8 java and remove openjdk7
   yum -y install java-1.8.0-openjdk
-  echo '*******removing java openjdk7********'
-  yum -y remove java-1.7.0-openjdk
+  #echo '*******removing java openjdk7********'
+  #yum -y remove java-1.7.0-openjdk
   # Java 8 (Oracle)
-  wget -q --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/jdk-8u131-linux-x64.rpm
-  rpm -Uvh jdk-8*
-  rm -f jdk-8*
+  #wget -q --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/jdk-8u131-linux-x64.rpm
+  #rpm -Uvh jdk-8*
+  #rm -f jdk-8*
   ## make java 8 default with the alternatives command
-  sudo alternatives  --set java /usr/java/jdk1.8.0_131/jre/bin/java
+  #sudo alternatives  --set java /usr/java/jdk1.8.0_131/jre/bin/java
   ## Set JAVA_HOME variable both now and for when the system restarts
   export JAVA_HOME
 else
   #------------------ if RHEL 7
-  yum -y install java-1.8.0-oracle  java-1.8.0-oracle-devel
+  #yum -y install java-1.8.0-oracle  java-1.8.0-oracle-devel
 fi
 
 # Maven
