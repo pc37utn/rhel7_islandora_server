fb86dcda94969bc6c6161850f4867746dfda112e
pc37utn <pc37utn@users.noreply.github.com>
2019-03-01 16:23:15 -0500

    add new php version

--- ss-2-apache-php.sh
+++ ss-2-apache-php.sh
@@ -22,6 +22,25 @@ usermod -a -G web tomcat
 # Apache configuration file
 #export APACHE_CONFIG_FILE=/etc/apache2/sites-enabled/000-default.conf
 
+# add epel repo
+sudo yum -y install epel-release
+sudo yum update
+
+# setup timezone
+sudo timedatectl set-timezone America/New_York
+
+# disable selinux
+sudo sed -i 's|SELINUX=enforcing$|SELINUX=disabled|' /etc/selinux/config
+sudo touch /.autorelabel
+
+# utilities and build tools
+sudo yum -y install wget mc zip unzip ntp psmisc gcc kernel-devel kernel-headers autoconf git vim htop tree mc zsh net-tools
+# recent (Jan 2019) changes have kept php 7.2 from working.
+# add remi repo and enable php56
+sudo yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+sudo yum -y install yum-plugin-priorities
+sudo yum-config-manager --enable remi-php56
+
 # Drush and drupal deps
 yum -y install php-gd php-devel php-xml php-soap php-curl
 yum -y install php-pecl-imagick ImageMagick perl-Image-ExifTool bibutils poppler-utils
